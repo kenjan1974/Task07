32d439eeebcd83ffcc2814a1ce488caa31487f93
jinx <jinx@stu.edu.tw>
2025-04-24 17:03:00 +0800

    1. 從 聯合新聞網取得焦點新聞連結，然後存到list 2. 從url list取得每個連結內的新聞內容

---
+++ task09_01.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 取得主頁面
+response = requests.get('https://udn.com/news/breaknews/1')
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# 找出所有新聞連結
+news_links = []
+for h3 in soup.find_all('h3', class_='rounded-thumb__title'):
+    a_tag = h3.find('a', href=True)
+    if a_tag:
+        news_links.append('https://udn.com' + a_tag['href'])
+
+# 逐一請求每則新聞內容
+for link in news_links:
+    news_resp = requests.get(link)
+    news_soup = BeautifulSoup(news_resp.text, 'html.parser')
+    # 取得所有新聞段落內容
+    paragraphs = news_soup.find_all('div', class_='article-content__paragraph')
+    print(f'新聞連結: {link}')
+    for news_content in paragraphs:
+        print(news_content.get_text(strip=True))
+    print('-' * 40)
